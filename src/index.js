f87cda9480b3b2f14587240cf9cd31fd2486ef75
TanmayMachkar <tanmaymacho@gmail.com>
2023-11-15 09:56:45 +0530

    updates pushed

--- src/index.js
+++ src/index.js
@@ -4,13 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import ParticlesBg from 'particles-bg';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <div class = 'myclass'> 
-    <App />
-    <ParticlesBg color="#000000" num={100} type="cobweb" bg={true} />  
-  </div>
+root.render( 
+    <App />  
 );
 reportWebVitals();
